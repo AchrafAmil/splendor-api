3527ecd963653940489de2532eb4eb89504401c2
Achraf Amil <achraf.amil@gmail.com>
2019-07-27 00:37:02 +0200

    Implement minimalist print logger

--- src/main/kotlin/com/neogineer/splendor/api/GameMaster.kt
+++ src/main/kotlin/com/neogineer/splendor/api/GameMaster.kt
@@ -1,13 +1,18 @@
 package com.neogineer.splendor.api
 
 import com.neogineer.splendor.api.data.NameAlreadyTakenException
+import com.neogineer.splendor.api.utils.Logger
+import com.neogineer.splendor.api.utils.PrintLogger
 import java.lang.IllegalStateException
 
 class GameMaster {
 
+    private val logger: Logger = PrintLogger()
+
     private val players = mutableListOf<Player>()
 
     fun registerPlayer(player: Player) {
+        logger.i(LOG_TAG, "registering player: ${player.name}")
         when {
             players.size >= 4 ->
                 throw IllegalStateException("can not add more than 4 players")
