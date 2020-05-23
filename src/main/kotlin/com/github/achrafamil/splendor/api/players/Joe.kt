057abcd47e1fb925ed72dc6d686827626089390b
achrafamil <achraf.amil@gmail.com>
2020-05-23 23:39:15 +0200

    Create Joe player

---
+++ src/main/kotlin/com/github/achrafamil/splendor/api/players/Joe.kt
@@ -0,0 +1,9 @@
+package com.github.achrafamil.splendor.api.players
+
+/**
+ * Joe is literally just a basic player.
+ * @see [BasicPlayer] for strategy.
+ *
+ */
+
+class Joe(name: String = "") : BasicPlayer("Joe $name")
